dc2f3a4ef81f75d76a778dfafc7c433a43e8fbf4
BudeureounGureum <matheo.bouiller@universite-paris-saclay.fr>
2026-06-03 19:59:43 +0200

    Added units to variable names and transformed wind direction to sin and cos values. Also filled missing values of wind direction with the previous (or the following when not available) day's value.

--- src/preprocessing/parse_weather.py
+++ src/preprocessing/parse_weather.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 def parse_weather_csv():
     input_file = Path("data/raw/weather_20201101_20260519.csv")
@@ -46,38 +47,41 @@ def parse_weather_csv():
     korean_to_english = {
         '지점명': 'location_name',
         '일시': 'date',
-        '평균기온(°C)': 'avg_temperature',
-        '최저기온(°C)': 'min_temperature',
-        '최고기온(°C)': 'max_temperature',
-        '강수 계속시간(hr)': 'precipitation_duration_hr',
-        '일강수량(mm)': 'daily_precipitation',
-        '최대 순간 풍속(m/s)': 'max_gust_wind_speed',
-        '최대 풍속(m/s)': 'max_wind_speed',
-        '평균 풍속(m/s)': 'avg_wind_speed',
+        '평균기온(°C)': 'avg_temperature(°C)',
+        '최저기온(°C)': 'min_temperature(°C)',
+        '최고기온(°C)': 'max_temperature(°C)',
+        '강수 계속시간(hr)': 'precipitation_duration(hr)',
+        '일강수량(mm)': 'daily_precipitation(mm)',
+        '최대 순간 풍속(m/s)': 'max_gust_wind_speed(m/s)',
+        '최대 풍속(m/s)': 'max_wind_speed(m/s)',
+        '평균 풍속(m/s)': 'avg_wind_speed(m/s)',
         '최다풍향(16방위)': 'most_frequent_wind_direction',
-        '평균 이슬점온도(°C)': 'avg_dew_point',
-        '최소 상대습도(%)': 'min_relative_humidity',
-        '평균 상대습도(%)': 'avg_humidity',
-        '평균 현지기압(hPa)': 'avg_local_pressure',
-        '최고 해면기압(hPa)': 'max_sea_level_pressure',
-        '최저 해면기압(hPa)': 'min_sea_level_pressure',
-        '평균 해면기압(hPa)': 'avg_sea_level_pressure',
+        '평균 이슬점온도(°C)': 'avg_dew_point(°C)',
+        '최소 상대습도(%)': 'min_relative_humidity(%)',
+        '평균 상대습도(%)': 'avg_humidity(%)',
+        '평균 현지기압(hPa)': 'avg_local_pressure(hPa)',
+        '최고 해면기압(hPa)': 'max_sea_level_pressure(hPa)',
+        '최저 해면기압(hPa)': 'min_sea_level_pressure(hPa)',
+        '평균 해면기압(hPa)': 'avg_sea_level_pressure(hPa)',
         '가조시간(hr)': 'sunshine_possible_hours',
         '합계 일조시간(hr)': 'total_sunshine_hours',
-        '합계 일사량(MJ/m2)': 'total_solar_radiation',
-        '일 최심신적설(cm)': 'daily_max_new_snow_depth',
-        '평균 전운량(1/10)': 'avg_cloud_cover',
-        '안개 계속시간(hr)': 'fog_duration_hr',
+        '합계 일사량(MJ/m2)': 'total_solar_radiation(MJ/m2)',
+        '일 최심신적설(cm)': 'daily_max_new_snow_depth(cm)',
+        '평균 전운량(1/10)': 'avg_cloud_cover(1/10)',
+        '안개 계속시간(hr)': 'fog_duration(hr)',
     }
     
     df = df.rename(columns=korean_to_english)
     
     # Convert fog_duration_hr to boolean indicating presence of fog to match the forecast data.
-    df['fog_duration_hr'] = df['fog_duration_hr'].notna()
-    df.rename(columns={'fog_duration_hr': 'fog_occured'}, inplace=True)
+    df['fog_duration(hr)'] = df['fog_duration(hr)'].notna()
+    df.rename(columns={'fog_duration(hr)': 'fog_occured'}, inplace=True)
     
     # Convert date column to datetime
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
+
+    # Sort by date
+    df = df.sort_values(['date']).reset_index(drop=True)
     
     # Convert all numeric columns (exclude string columns)
     for col in df.columns:
@@ -97,24 +101,32 @@ def parse_weather_csv():
         null_count = df[col].isna().sum()
         if null_count > 0:
             print(f"{col}: {null_count} missing values")
-            
+
     # Fill event columns where NaN means "did not happen"
     zero_fill_cols = [
-        'precipitation_duration_hr', 'max_10min_precipitation', 'max_1hr_precipitation',
-        'daily_precipitation', 'daily_max_new_snow_depth', 'daily_max_snow_depth',
-        'total_3hr_new_snow', 'precipitation_9to9', 'fog_duration_hr'
+        'precipitation_duration(hr)', 'daily_precipitation(mm)', 'daily_max_new_snow_depth(cm)'
     ]
 
     for col in zero_fill_cols:
         if col in df.columns:
             # Existing 0.0 means "Trace amount". 
             # The smallest non-zero values in these columns are 0.08 to 0.17 depending on the column.
-            # We change "trace amount" 0s to 0.01 so XGBoost can distinguish it from 0.0 (no event).
+            # We change "trace amount" 0s to 0.01 so that the algorithm can distinguish it from 0.0 (no event).
             mask_zero = df[col] == 0
             df.loc[mask_zero, col] = 0.01
             
             # Fill NaN (nothing happened) with 0.0
             df[col] = df[col].fillna(0)
+
+    # For the wind direction, we use the previous day's value (or the next day's value when at the begining of the list)
+    df['most_frequent_wind_direction'] = (df['most_frequent_wind_direction'].ffill().bfill())
+
+    radians = np.deg2rad(df['most_frequent_wind_direction'])
+
+    df['most_frequent_wind_direction_sin'] = np.sin(radians)
+    df['most_frequent_wind_direction_cos'] = np.cos(radians)
+
+    df.drop(columns=['most_frequent_wind_direction'], inplace=True)
             
     # For continuous numeric columns (temperature, wind, pressure, evaporation, etc.), 
     # we use 'pchip' (Piecewise Cubic Hermite Interpolating Polynomial) which is more
@@ -127,9 +139,6 @@ def parse_weather_csv():
     df[numeric_cols] = df.groupby('location_name')[numeric_cols].transform(
         lambda x: x.interpolate(method='linear', limit_direction='both')
     )
-
-    # Sort by date
-    df = df.sort_values(['date']).reset_index(drop=True)
     
     # Save to parquet
     df.to_parquet(
